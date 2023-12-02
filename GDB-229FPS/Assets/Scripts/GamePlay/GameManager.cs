e012560fb5527ab1a8a4df5e46751fd482855510
Vivianne Zsoldos <vdzsoldos@student.fullsail.edu>
2023-12-02 02:08:11 -0500

    Started UI
    Updated Game Manager

--- GDB-229FPS/Assets/Scripts/GamePlay/GameManager.cs
+++ GDB-229FPS/Assets/Scripts/GamePlay/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,13 +9,52 @@ public class GameManager : MonoBehaviour
     public GameObject Player;
     public Movement PlayerScript;
     public float Gravity;
+    int enemiesRemaining;
+    public bool isPaused;
+
+    [SerializeField] TMP_Text enemyCountText;
+
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+
+
+
     void Awake() {
         instance = this;
         Player = GameObject.FindWithTag("Player");
         PlayerScript = GetComponent<Movement>();
         //Gravity = PlayerScript.GetGravity();
     }
+    
     void Update() {
         
     }
+
+    public void StatePaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0; //Pauses everything- Minus menu
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+     public void UpdateGameGoal(int amount)
+    {
+        enemiesRemaining += amount;
+        enemyCountText.text = enemiesRemaining.ToString("0");
+
+        if (enemiesRemaining <= 0)
+        {
+            //You win!
+            StatePaused();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+            
+        }
+    }
+
 }
