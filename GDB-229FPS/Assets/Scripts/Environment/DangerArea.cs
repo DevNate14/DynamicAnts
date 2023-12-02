da53f80afc44a774d973936d645cde549dfc4c89
TyMill921 <tdmiller1@student.fullsail.edu>
2023-12-02 02:04:17 -0500

    Small changes
    Better platform prefab, lava trap started

--- GDB-229FPS/Assets/Scripts/Environment/DangerArea.cs
+++ GDB-229FPS/Assets/Scripts/Environment/DangerArea.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class DangerArea : MonoBehaviour
 {
+    [SerializeField] int damageOverTime;
+    int delayDamage;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    void OnTriggerStay(Collider other)
+    {
+        if(other.isTrigger){
+            return;
+        }
+
+        IDamageable thing = other.GetComponent<IDamageable>();
+        if(thing != null){
+            thing.Damage(damageOverTime);
+        }
     }
 }
