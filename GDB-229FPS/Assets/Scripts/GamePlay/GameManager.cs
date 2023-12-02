258fbde07513e736753d87af4f2495c9332eb2c5
Vivianne Zsoldos <vdzsoldos@student.fullsail.edu>
2023-12-02 05:31:51 -0500

    Added W/L UIs
    Updated Prefabs

--- GDB-229FPS/Assets/Scripts/GamePlay/GameManager.cs
+++ GDB-229FPS/Assets/Scripts/GamePlay/GameManager.cs
@@ -76,4 +76,12 @@ public class GameManager : MonoBehaviour
         }
      }
 
+     public void YouLose()
+    {
+        StatePaused();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
 }
+
+
