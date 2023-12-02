e8a998346eba54b291fb39c6d3ad2945e3cb056b
Vivianne Zsoldos <vdzsoldos@student.fullsail.edu>
2023-12-02 05:44:32 -0500

    Revert "Merge branch 'main' of https://github.com/DevNate14/DynamicAnts"
    This reverts commit fd62cd3c115f731756685dfe37422442a7fb8057, reversing
    changes made to 9fbd2b7cfcc111c959e8e15b4088aedc034b8e4a.

--- GDB-229FPS/Assets/Scripts/Enemy/EnemyAI.cs
+++ GDB-229FPS/Assets/Scripts/Enemy/EnemyAI.cs
@@ -13,7 +13,6 @@ public class EnemyAI : MonoBehaviour, IDamageable
     [SerializeField] GameObject bullet;
     [SerializeField] Transform headPosition;
     [SerializeField] int viewCone;
-    [SerializeField] int turnSpeed;
     Vector3 playerDirection;
     bool playerInRange;
     bool shooting;
@@ -41,9 +40,7 @@ public class EnemyAI : MonoBehaviour, IDamageable
             if(hit.collider.CompareTag("Player") && angleToPlayer <= viewCone){
                 
                 agent.SetDestination(GameManager.instance.player.transform.position);
-                if(agent.remainingDistance < agent.stoppingDistance){
-                    faceTarget();
-                }
+
                 if(!shooting){
                     StartCoroutine(shoot());
                 }
@@ -53,10 +50,6 @@ public class EnemyAI : MonoBehaviour, IDamageable
         return false;
     }
 
-    void faceTarget(){
-        Quaternion rotate = Quaternion.LookRotation(playerDirection);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotate, Time.deltaTime * turnSpeed);
-    }
 
     public void Damage(int amount)
     {
