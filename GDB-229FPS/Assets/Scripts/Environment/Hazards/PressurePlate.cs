4c4d3f74bc8bd46b14ea54a4904dda360812ce35
Dante Williams <DRWilliams5@student.fullsail.edu>
2024-01-18 20:59:05 -0500

    pressure plate and treadmill both have an optional rework

--- GDB-229FPS/Assets/Scripts/Environment/Hazards/PressurePlate.cs
+++ GDB-229FPS/Assets/Scripts/Environment/Hazards/PressurePlate.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class PressurePlate : MonoBehaviour
 {
     [SerializeField] GameObject trap;
+    [SerializeField] Collider Hitbox;
+    [SerializeField] bool RaycastMode;
     public bool hit;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        if (RaycastMode)
+        {
+            Hitbox.enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!hit) //just so this triggers only once
+        if (RaycastMode && !hit) //just so this triggers only once
         {
             Debug.DrawRay(transform.position, transform.up * 1, Color.red);
             if (Physics.Raycast(transform.position, transform.up, 1))
@@ -26,5 +31,12 @@ public class PressurePlate : MonoBehaviour
                 transform.position = transform.position - transform.up * 0.08f; // pressure plate sinks (a little too far) into the floor
             }
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hit = true;
+        Instantiate(trap, transform.position + transform.up * 10, transform.rotation); // spawn a boulder above
+        transform.position = transform.position - transform.up * 0.08f; // pressure plate sinks (a little too far) into the floor
     }
 }
