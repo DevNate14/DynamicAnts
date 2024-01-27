6895976c21a16925261a63e6bda81c11dce9294e
Kenneth Fitzpatrick <Kennethfitzpatrick55@gmail.com>
2024-01-26 22:54:21 -0500

    key update incomplete

--- GDB-229FPS/Assets/Scripts/Environment/Objects/Podium.cs
+++ GDB-229FPS/Assets/Scripts/Environment/Objects/Podium.cs
@@ -1,18 +1,63 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Podium : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    //black //green
+    [SerializeField] Color color1, color2;
+    [SerializeField] Renderer[] bulbs;
+    [SerializeField] GameObject[] Keeys;
+    private void Start()
     {
-        
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Inventory bag = player.GetComponent<Inventory>();
+        CheckKeys(bag);
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool CheckKeys(Inventory keys)
     {
-        
+        Keeys[0].SetActive(keys.CheckKeyById(1));
+        if(keys.CheckKeyById(1) ) 
+        {
+            bulbs[0].material.color = color2;
+            
+        }
+        else
+        {
+            bulbs[0].material.color = color1;
+        }
+        Keeys[1].SetActive(keys.CheckKeyById(2));
+        if (keys.CheckKeyById(2))
+        {
+            bulbs[1].material.color = color2;
+        }
+        else
+        {
+            bulbs[1].material.color = color1;
+        }
+        Keeys[2].SetActive(keys.CheckKeyById(3));
+        if (keys.CheckKeyById(3))
+        {
+            bulbs[2].material.color = color2;
+        }
+        else
+        {
+            bulbs[2].material.color = color1;
+        }
+
+        if (keys.CheckKeys())
+        {
+            return true;
+
+        }
+        else 
+        {
+            return false;
+        }
+
     }
 }
